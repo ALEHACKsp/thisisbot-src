9d36ee9cc8c127ae82bf3eebc1eeb038b5263212
ThisIsRico8696 <72346329+HackerGod8696@users.noreply.github.com>
2021-06-08 14:41:55 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ "verification": "",
+ "description": "", //description
+ "domain": "", // domain
+ "google_analitics": "", // google analitics
+ "token": process.env.TOKEN,
+ "https":"https://", // leave as is
+ "port":"5003",
+
+ "client_id":"", // bot client ID
+ "secret":""// bot client secret for auth
+
+}
